7fc2b885fe10fb6e75930f8c13d5a1275942a2f2
Jay Barr <jayrbarr@gmail.com>
2020-03-25 10:37:25 -0700

    Passes stringifyJSON tests.

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,36 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  switch (typeof obj) {
+  case 'number':
+  case 'boolean':
+    return '' + obj;
+  case 'string':
+    return '"' + obj + '"';
+  case 'function':
+  case 'undefined':
+    break;
+  default:
+    if (Array.isArray(obj)) {
+      return stringifyArray(obj);
+    }
+    if (obj === null) {
+      return 'null';
+    }
+    return stringifyObject(obj);
+  }
+};
+
+var stringifyArray = function(arr) {
+  return '[' + arr.map((item) => stringifyJSON(item)).join(',') + ']';
+};
+
+var stringifyObject = function(obj) {
+  var tempArr = [];
+  for (let key in obj) {
+    if (typeof obj[key] !== 'undefined' && typeof obj[key] !== 'function') {
+      tempArr.push('"' + key + '":' + stringifyJSON(obj[key]));
+    }
+  }
+  return '{' + tempArr.join(',') + '}';
 };
